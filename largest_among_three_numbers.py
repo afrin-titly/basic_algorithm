6fb6ab899604bb4858763ca12d5d84a90efbba1b
afrin <afrin.deeba11114@gmail.com>
2019-01-22 16:29:29 +0900

    solve largest among three numbers problem

---
+++ largest_among_three_numbers.py
@@ -0,0 +1,14 @@
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+
+if a>b:
+    if a>b:
+        print(a," is the largest number")
+    else:
+        print(c," is the largest number")
+else:
+    if b>c:
+        print(b," is the largest number")
+    else:
+        print(c," is the largest number")
